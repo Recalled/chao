80942ad3458b4bcd502116a77c5d1c620d735e4e
Lian.Qian Chao <i@lchao.cc>
2019-02-24 22:40:10 +0800

    first commit!!

---
+++ src/main/java/cc/lchao/chao/ChaoApplication.java
@@ -0,0 +1,13 @@
+package cc.lchao.chao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaoApplication.class, args);
+    }
+
+}
